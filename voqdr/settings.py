af180eb6cf450e6ce25849eb2432d82884ebe3d2
hassanarifbinarif <hassanarif2811@gmail.com>
2023-01-11 17:04:51 +0500

    DBSqlite3 added

--- voqdr/settings.py
+++ voqdr/settings.py
@@ -82,14 +82,14 @@ WSGI_APPLICATION = 'voqdr.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "voqdr",
-        "USER": "root",
-        "PASSWORD": "",
-        "HOST": "127.0.0.1",
-        "PORT": "3306"
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # "ENGINE": "django.db.backends.mysql",
+        # "NAME": "voqdr",
+        # "USER": "root",
+        # "PASSWORD": "",
+        # "HOST": "127.0.0.1",
+        # "PORT": "3306"
     }
 }
 
