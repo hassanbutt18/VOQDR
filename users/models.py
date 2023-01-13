6b7e0eeb5e18db625dad3892916ba30b3f8a6292
hassanarifbinarif <hassanarif2811@gmail.com>
2023-01-13 15:30:33 +0500

    settings.py updated

--- users/models.py
+++ users/models.py
@@ -62,17 +62,6 @@ class SharedOrganization(models.Model):
         return self.shared_to
 
 
-
-class Dummmy(models.Model):
-    shared_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='shared_by_organization')
-    shared_to = models.EmailField(null=True, blank=True)
-    role = models.CharField(max_length=15, default=UserRoles.VIEWER)
-    is_verified = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.shared_to
 class InvitedOrganization(models.Model):
     shared_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='invite_by_organization')
     shared_to = models.EmailField(null=True, blank=True)
