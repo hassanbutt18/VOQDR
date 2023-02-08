c50c898b74b2b734b714fff18a73a2d0aee12ef7
Hassan <89502754+hassanbutt18@users.noreply.github.com>
2023-02-08 12:04:24 +0500

    Update common.js

--- static/web/Assets/JS/common.js
+++ static/web/Assets/JS/common.js
@@ -1,3 +1,50 @@
+function dateTimeTominutes(datetime){
+    var date = new Date(datetime);
+    var seconds = date.getTime() / 1000;
+    var minutes = seconds / 60;
+    return minutes
+}
+
+function filterObjects(objects) {
+    for (let i = 0; i < objects.length - 1; i++) {
+      for (let j = i + 1; j < objects.length; j++) {
+        if (objects[i]["insertedAt"].split('T')[1].substr(0, 4) == objects[j]["insertedAt"].split('T')[1].substr(0, 4)) {
+          let time1 = parseFloat(objects[i]["insertedAt"].split('T')[1].substr(3, 5).replace(':', '.'));
+          let time2 = parseFloat(objects[j]["insertedAt"].split('T')[1].substr(3, 5).replace(':', '.'));
+          if (time1 > time2) {
+            objects.splice(j, 1);
+          } else {
+            objects.splice(i, 1);
+          }
+        }
+      }
+    }
+    return objects.slice(0, 10);
+}
+
+
+function sortServiceType(array){
+    let sortedArray = filterObjects(array);
+    return sortedArray.sort((a,b)=>{
+        // let serviceType = obj.serviceType;
+        // return serviceType === "GPS" || serviceType === "MCELL" || serviceType === "SCELL";
+        let order = ["GPS", "MCELL", "SCELL"];
+        let aIndex = order.indexOf(a);
+        let bIndex = order.indexOf(b);
+        return aIndex - bIndex;
+    })
+}
+function filterTimeDifference(timeDifference, myArray){
+    return myArray.filter((event, index, arr) => {
+        let prevEvent = arr[index - 1];
+        if (index === 0 || !prevEvent) return true;
+        
+        let eventTime = new Date(event.timestamp).getTime();
+        let prevEventTime = new Date(prevEvent.timestamp).getTime();
+        
+        return eventTime - prevEventTime >= timeDifference * 60 * 1000;
+    });
+}
 function formDataToObject(formData) {
     let getData = {}
     formData.forEach(function(value, key) {
