b8695f35018d03b2535f8d357ee4d381c6f310b4
hassanarifbinarif <hassanarif2811@gmail.com>
2023-02-22 18:56:03 +0500

    Navbar layout fixed for safari browsers

--- static/web/Assets/JS/navbar.js
+++ static/web/Assets/JS/navbar.js
@@ -1,10 +1,10 @@
 // Navbar controls
 
-const menu = document.querySelector(".nav-links");
-const menuItems = document.querySelectorAll(".nav-items");
-const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".navbar-collapse");
+// const menuItems = document.querySelectorAll(".nav-items");
+// const hamburger = document.querySelector(".navbar-toggler");
 const closeIcon = document.querySelector(".closeIcon");
-closeIcon.style.display = "none";
+closeIcon.style.display = "none";  
 const menuIcon = document.querySelector(".menuIcon");
 
 // menuItems.forEach((item, i) => {
@@ -13,28 +13,27 @@ const menuIcon = document.querySelector(".menuIcon");
 // });
 
 function toggleMenu() {
-  if (menu.classList.contains("showMenu")) {
-    menu.classList.remove("showMenu");
-    hamburger.classList.remove("hamburger-enabled");
+  if (menu.classList.contains("show")) {
+    // hamburger.classList.remove("hamburger-enabled");
     closeIcon.style.display = "none";
     menuIcon.style.display = "block";
   } else {
-    menu.classList.add("showMenu");
+    menu.classList.add("show");
     closeIcon.style.display = "block";
-    hamburger.classList.add("hamburger-enabled");
+    // hamburger.classList.add("hamburger-enabled");
     menuIcon.style.display = "none";
   }
 }
 
-hamburger.addEventListener("click", toggleMenu);
+// hamburger.addEventListener("click", toggleMenu);
 
-menuItems.forEach(function (menuItem) {
-  menuItem.addEventListener("click", toggleMenu);
-});
+// menuItems.forEach(function (menuItem) {
+//   menuItem.addEventListener("click", toggleMenu);
+// });
 
 
-addEventListener('resize', function(event) {
-  if(this.window.innerWidth > 768 && menu.classList.contains("showMenu")) {
-    toggleMenu();
-  }
-})
+// addEventListener('resize', function(event) {
+//   if(this.window.innerWidth > 768 && menu.classList.contains("showMenu")) {
+//     toggleMenu();
+//   }
+// })
