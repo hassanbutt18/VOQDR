194c099afe8dcf0d4d8d16211747d836bee18b77
hassanarifbinarif <hassanarif2811@gmail.com>
2023-03-13 10:47:37 +0500

    pos save signal for organiation permissions changed

--- users/models.py
+++ users/models.py
@@ -248,17 +248,17 @@ def postdelete_handler(sender, instance, **kwargs):
     User.objects.filter(id=instance.shared_to_id).delete()
 
 
-@receiver(models.signals.pre_save, sender=OrganizationPermissions)
-def on_shared_by_update_handler(sender, instance, **kwargs):
-    print("pre_save event called")
-    org = OrganizationPermissions.objects.filter(shared_to=instance.shared_to).first()
-    if org:
-        if org.shared_by_id is not instance.shared_by_id:
-            FavouriteDevice.objects.filter(user_id=instance.shared_to_id).delete()
-            devices = FavouriteDevice.objects.filter(user=instance.shared_by)
-            if devices:
-                for obj in devices:
-                    FavouriteDevice.objects.create(user=instance.shared_to, device_id=obj.device_id)
+# @receiver(models.signals.pre_save, sender=OrganizationPermissions)
+# def on_shared_by_update_handler(sender, instance, **kwargs):
+#     print("pre_save event called")
+#     org = OrganizationPermissions.objects.filter(shared_to=instance.shared_to).first()
+#     if org:
+#         if org.shared_by_id is not instance.shared_by_id:
+#             FavouriteDevice.objects.filter(user_id=instance.shared_to_id).delete()
+#             devices = FavouriteDevice.objects.filter(user=instance.shared_by)
+#             if devices:
+#                 for obj in devices:
+#                     FavouriteDevice.objects.create(user=instance.shared_to, device_id=obj.device_id)
 
 @receiver(models.signals.post_save, sender=OrganizationPermissions)
 def on_shared_by_update_handler(sender, instance, created, **kwargs):
@@ -268,6 +268,11 @@ def on_shared_by_update_handler(sender, instance, created, **kwargs):
         if shared_org.shared_by != instance.shared_by:
             shared_org.shared_by = instance.shared_by
             shared_org.save(update_fields=['shared_by'])
+            FavouriteDevice.objects.filter(user_id=instance.shared_to_id).delete()
+            devices = FavouriteDevice.objects.filter(user=instance.shared_by)
+            if devices:
+                for obj in devices:
+                    FavouriteDevice.objects.create(user=instance.shared_to, device_id=obj.device_id)
         if shared_org.role != instance.role:
             shared_org.role = instance.role
             shared_org.save(update_fields=['role'])
